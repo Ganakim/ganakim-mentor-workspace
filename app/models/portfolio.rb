8b310a98191e7ca72a0e25d0089a1448508f3073
Ganakim <espene.cornwall@gmail.com>
2017-07-19 16:39:26 +0000

    custom scope portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,4 +3,11 @@ class Portfolio < ApplicationRecord
   
   scope :js, -> {where(subtitle: 'JavaScript')}
   scope :ruby, -> {where(subtitle: 'Ruby')}
+  
+  efter_initialize :set_defaults
+  
+  def set_defaults
+    self.main_image ||= "http://via.placeholder.com/700x400"
+    self.thumb_image ||= "http://via.placeholder.com/350x200"
+  end
 end
